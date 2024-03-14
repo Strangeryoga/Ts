6087381b774e2013d77d9d5d4cb408ebd546b9bd
Tejas Doke <55567984+Strangeryoga@users.noreply.github.com>
2024-03-14 18:01:37 +0000

    union

---
+++ 02basics/myUnion.js
@@ -0,0 +1,10 @@
+var score = 33;
+score = 44;
+score = "55";
+var stranger = { name: "stranger", id: 32 };
+stranger = { username: "st", id: 92 };
+function getDbId(id) {
+    console.log("DB id is: ".concat(id));
+}
+getDbId(3);
+getDbId("3");
