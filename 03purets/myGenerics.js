1219e4a0d9ffd7ef98d9aa33d3ff8f6e2ae75e3a
Tejas Doke <55567984+Strangeryoga@users.noreply.github.com>
2024-03-31 17:37:39 +0000

    generics

---
+++ 03purets/myGenerics.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var score = [];
+var names = [];
+function identityOne(val) {
+    return val;
+}
+function identityTwo(val) {
+    return val;
+}
+function identityThree(val) {
+    return val;
+}
+// identityThree(3)
+function identityFour(val) {
+    return val;
+}
+identityFour({ brand: "gym", type: 3 });
