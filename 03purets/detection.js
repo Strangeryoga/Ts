b2852734ff6e1c4f5b49da3c4a7a8080674fe3d4
Tejas Doke <55567984+Strangeryoga@users.noreply.github.com>
2024-03-31 18:18:26 +0000

    typeof type guards

---
+++ 03purets/detection.js
@@ -0,0 +1,28 @@
+"use strict";
+// Type Narrowing
+Object.defineProperty(exports, "__esModule", { value: true });
+function detectType(val) {
+    if (typeof val === "string")
+        return val.toLowerCase();
+    return val + 3;
+}
+function provideId(id) {
+    if (!id) {
+        console.log("Please provide Id");
+        return;
+    }
+    id.toLowerCase();
+}
+function printAll(strs) {
+    if (strs) {
+        if (typeof strs === "object") {
+            for (var _i = 0, strs_1 = strs; _i < strs_1.length; _i++) {
+                var s = strs_1[_i];
+                console.log(s);
+            }
+        }
+        else if (typeof strs === "string") {
+            console.log(strs);
+        }
+    }
+}
