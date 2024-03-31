a6ed5c6b76281a27fee16cbb76bfba54ac8327b4
Tejas Doke <55567984+Strangeryoga@users.noreply.github.com>
2024-03-31 18:32:43 +0000

    instanceof and type

--- 03purets/detection.js
+++ 03purets/detection.js
@@ -26,3 +26,25 @@ function printAll(strs) {
         }
     }
 }
+// instanceof
+function logValue(x) {
+    if (x instanceof Date) {
+        console.log(x.toUTCString());
+    }
+    else {
+        console.log(x.toUpperCase());
+    }
+}
+function isFish(pet) {
+    return pet.swim !== undefined;
+}
+function getFood(pet) {
+    if (isFish(pet)) {
+        pet;
+        return "fish food";
+    }
+    else {
+        pet;
+        return "bird Food";
+    }
+}
--- 03purets/detection.ts
+++ 03purets/detection.ts
@@ -29,4 +29,36 @@ function printAll(strs: string | string[] | null) {
 }
 
 
+// instanceof
+function logValue(x: Date | string) {
+    if (x instanceof Date) {
+        console.log(x.toUTCString());
+    } else {
+        console.log(x.toUpperCase());
+    }
+}
+
+
+// type
+type Fish = {swim: () => void};
+type Bird = {fly: () => void};
+
+function isFish(pet: Fish | Bird): pet is Fish{
+    return (pet as Fish).swim !== undefined
+}
+
+function getFood(pet: Fish | Bird){
+    if (isFish(pet)) {
+        pet
+        return "fish food"
+    } else {
+        pet
+        return "bird Food"
+    }
+}
+
+
+
+
+
 export {}
